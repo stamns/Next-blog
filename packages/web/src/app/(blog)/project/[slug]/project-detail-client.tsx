8eee0d81cbeb72572d7727fb68761745fc7eee5a
Anvika Bettencourt <github@861861.xyz>
2025-12-22 16:08:43 +0800

    更新部分功能代码

--- packages/web/src/app/(blog)/project/[slug]/project-detail-client.tsx
+++ packages/web/src/app/(blog)/project/[slug]/project-detail-client.tsx
@@ -12,14 +12,10 @@ interface Props {
 export function ProjectDetailClient({ project }: Props) {
   const { themeName, themeConfig } = useThemeContext();
   const theme = getTheme(themeName);
-  const { BlogLayout } = theme;
 
   // 使用主题自定义的 ProjectDetail 组件，如果没有则使用默认组件
   const ProjectDetailComponent = theme.ProjectDetail || DefaultProjectDetail;
 
-  return (
-    <BlogLayout config={themeConfig}>
-      <ProjectDetailComponent project={project} config={themeConfig} />
-    </BlogLayout>
-  );
+  // 不需要再包裹 BlogLayout，因为 (blog)/layout.tsx 已经包裹过了
+  return <ProjectDetailComponent project={project} config={themeConfig} />;
 }
